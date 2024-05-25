2fbd51d855f0fd388024c4fbcb13213733be5a00
Jefte Elioenai Zamarron Pule <Jefte_Zamarron23@unikino.edu.mx>
2024-05-24 17:43:11 -0700

    Implementando Colas con arreglos

--- App/Program.cs
+++ App/Program.cs
@@ -1,19 +1,38 @@
 ﻿Arreglos.Pilas pila = new Arreglos.Pilas(10);
-Console.WriteLine(pila.ObtenerDatos());
+Arreglos.Colas cola = new Arreglos.Colas(10);
+
+Console.WriteLine(cola.ObtenerDatos());
+Console.WriteLine("-------");
+cola.Agregar("A");
+cola.Agregar("B");
+cola.Agregar("C");
+cola.Agregar("D");
+cola.Agregar("E");
+cola.Agregar("F");
+Console.WriteLine(cola.ObtenerDatos());
 Console.WriteLine("-------");
 
-pila.Agregar("A");
-pila.Agregar("B");
-pila.Agregar("C");
-pila.Agregar("D");
-pila.Agregar("E");
-pila.Agregar("F");
-Console.WriteLine(pila.ObtenerDatos());
+cola.Eliminar();
+Console.WriteLine(cola.ObtenerDatos());
+Console.WriteLine("-------");
+cola.Eliminar();
+Console.WriteLine(cola.ObtenerDatos());
 Console.WriteLine("-------");
 
-pila.Eliminar();
-Console.WriteLine(pila.ObtenerDatos());
-Console.WriteLine("-------");
-pila.Eliminar();
-Console.WriteLine(pila.ObtenerDatos());
-Console.WriteLine("-------");
+//Console.WriteLine(pila.ObtenerDatos());
+//Console.WriteLine("-------");
+//pila.Agregar("A");
+//pila.Agregar("B");
+//pila.Agregar("C");
+//pila.Agregar("D");
+//pila.Agregar("E");
+//pila.Agregar("F");
+//Console.WriteLine(pila.ObtenerDatos());
+//Console.WriteLine("-------");
+
+//pila.Eliminar();
+//Console.WriteLine(pila.ObtenerDatos());
+//Console.WriteLine("-------");
+//pila.Eliminar();
+//Console.WriteLine(pila.ObtenerDatos());
+//Console.WriteLine("-------");
