1bd5aa8bafb81849e722b6414f50b95aa9772b7c
Jefte Elioenai Zamarron Pule <Jefte_Zamarron23@unikino.edu.mx>
2024-06-06 07:53:07 -0700

    Terminando el formulario Arreglos Colas

--- Desktop/FrmArreglosColas.Designer.cs
+++ Desktop/FrmArreglosColas.Designer.cs
@@ -35,16 +35,19 @@
             btnObtenerDatos = new Button();
             label1 = new Label();
             txtAgregar = new TextBox();
+            label2 = new Label();
+            txtElementos = new TextBox();
             SuspendLayout();
             // 
             // btnIniciar
             // 
-            btnIniciar.Location = new Point(23, 25);
+            btnIniciar.Location = new Point(240, 52);
             btnIniciar.Name = "btnIniciar";
             btnIniciar.Size = new Size(75, 23);
-            btnIniciar.TabIndex = 0;
+            btnIniciar.TabIndex = 1;
             btnIniciar.Text = "Iniciar";
             btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click;
             // 
             // txtDatos
             // 
@@ -61,30 +64,33 @@
             // 
             // btnAgregar
             // 
-            btnAgregar.Location = new Point(216, 81);
+            btnAgregar.Location = new Point(240, 80);
             btnAgregar.Name = "btnAgregar";
             btnAgregar.Size = new Size(75, 23);
-            btnAgregar.TabIndex = 0;
+            btnAgregar.TabIndex = 3;
             btnAgregar.Text = "Agregar";
             btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
             // 
             // btnEliminar
             // 
             btnEliminar.Location = new Point(23, 136);
             btnEliminar.Name = "btnEliminar";
             btnEliminar.Size = new Size(75, 23);
-            btnEliminar.TabIndex = 0;
+            btnEliminar.TabIndex = 4;
             btnEliminar.Text = "Eliminar";
             btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
             // 
             // btnObtenerDatos
             // 
             btnObtenerDatos.Location = new Point(23, 191);
             btnObtenerDatos.Name = "btnObtenerDatos";
             btnObtenerDatos.Size = new Size(75, 23);
-            btnObtenerDatos.TabIndex = 0;
+            btnObtenerDatos.TabIndex = 5;
             btnObtenerDatos.Text = "Obtener";
             btnObtenerDatos.UseVisualStyleBackColor = true;
+            btnObtenerDatos.Click += btnObtenerDatos_Click;
             // 
             // label1
             // 
@@ -97,16 +103,34 @@
             // 
             // txtAgregar
             // 
-            txtAgregar.Location = new Point(108, 81);
+            txtAgregar.Location = new Point(133, 81);
             txtAgregar.Name = "txtAgregar";
             txtAgregar.Size = new Size(100, 23);
-            txtAgregar.TabIndex = 5;
+            txtAgregar.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(189, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Introduce el número de elementos";
+            // 
+            // txtElementos
+            // 
+            txtElementos.Location = new Point(207, 23);
+            txtElementos.Name = "txtElementos";
+            txtElementos.Size = new Size(133, 23);
+            txtElementos.TabIndex = 0;
             // 
             // FrmArreglosColas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(700, 338);
+            Controls.Add(txtElementos);
+            Controls.Add(label2);
             Controls.Add(txtAgregar);
             Controls.Add(label1);
             Controls.Add(txtDatos);
@@ -129,5 +153,7 @@
         private Button btnObtenerDatos;
         private Label label1;
         private TextBox txtAgregar;
+        private Label label2;
+        private TextBox txtElementos;
     }
 }
--- Desktop/FrmArreglosColas.cs
+++ Desktop/FrmArreglosColas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Arreglos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,78 @@ namespace Desktop
 {
     public partial class FrmArreglosColas : Form
     {
+        Colas _colas;
         public FrmArreglosColas()
         {
             InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool sePudoConvertir = false;
+                int valorConvertido = 0;
+                string elementos = txtElementos.Text;
+                sePudoConvertir = int.TryParse(elementos, out valorConvertido);
+                if (!sePudoConvertir)
+                {
+                    throw new Exception("no se pudo convertir");
+                }
+                _colas = new Colas(valorConvertido);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtDatos.Text = string.Empty;
+                _colas.Agregar(txtAgregar.Text);
+
+                txtDatos.Text = _colas.ObtenerDatos();
+                txtAgregar.Text = string.Empty;
+                txtAgregar.Focus();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtDatos.Text = string.Empty;
+                _colas.Eliminar();
+                txtDatos.Text = _colas.ObtenerDatos();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnObtenerDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtDatos.Text = string.Empty;
+                txtDatos.Text = _colas.ObtenerDatos();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
